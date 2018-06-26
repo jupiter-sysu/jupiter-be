8feea4f3ec45245a21a4fe4b87bc72d46e8bb3ca
zscd0218 <809722633@qq.com>
2018-06-26 23:27:03 +0800

    注册的api

---
+++ login/mobile_login/index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+function var_json($code, $enmsg, $cnmsg, $data) {
+    $out['code'] = $code;
+    $out['enmsg'] = $enmsg;
+    $out['cnmsg'] = $cnmsg;
+    $out['data'] = $data;
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($out, JSON_HEX_TAG);
+    exit(0);
+}
+function generateHash($password) {
+    if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
+        $salt = '$2y$11$' . substr(md5(uniqid(rand(), true)), 0, 22);
+        return crypt($password, $salt);
+    }
+}
+$post_body = file_get_contents('php://input');
+$result_json = json_decode($post_body);
+$phone = empty($result_json->phone_num) ? '' : $result_json->phone_num;
+$password = empty($result_json->password) ? '' : $result_json->password;
+$connect = mysql_connect("39.108.15.127", "root", "Jupiter");
+if (!$connect) {
+    die('Could not connect: ' . mysql_error());
+}
+mysql_select_db("Jupiter_db", $connect);
+$result = mysql_query("SELECT * FROM user_info_temp
+    WHERE phone_num=" . $phone);
+$row = mysql_fetch_array($result);
+if (!$row) {
+    var_json(500, 'unregistered_phone', '该手机号未注册', null);
+} else {
+    if ($row['crypted_password'] == generateHash($password)) {
+        $data['user_id'] = $row['user_id'];
+        $data['user_name'] = $row['user_name'];
+        $data['authority'] = $row['authority'];
+        var_json(200, 'ok', '成功', $data);
+        $_SESSION['phone_num'] = $phone;
+    } else {
+        var_json(500, 'wrong_password', '密码错误', null);
+    }
+}
+?>
