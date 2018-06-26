deac3dce674491f134e4c4b9b693ea503662e400
WWWWEEEEIIII <934406854@qq.com>
2018-06-27 01:13:51 +0800

    增加了图片管理凭证的接口
    This reverts commit a825294a74bc84bd47f388474ba8778867f438a9.

---
+++ token/get/index.php
@@ -0,0 +1,27 @@
+﻿<?php
+require_once '/alidata/www/phpwind/Jupiter/php-sdk-master/php-sdk-master/autoload.php';
+
+use \Qiniu\Auth;
+
+$accessKey = 'mVNzLSKB_XpOpfVy9Xn3tvpCgmu9iiwSIbr8EujL';
+$secretKey = '4IX1fpCwivK0EN8mCA3lReTqKdREg2R0bXVmIjtY';
+$bucket = 'jupiter';
+
+$auth = new Auth($accessKey, $secretKey);
+
+$expires = 3600;
+
+$policy = null;
+$upToken = $auth->uploadToken($bucket, null, $expires, $policy, true);
+
+function var_json($error_code, $data) {
+	$out['code'] = $error_code ?: 0;
+	$out['cnmsg'] = '成功';
+	$out['enmsg'] = 'ok';
+    $out['data'] = $data ?: '';
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($out, JSON_HEX_TAG);
+    exit(0);
+}
+$data['token'] = $upToken;
+var_json(200, $data);
